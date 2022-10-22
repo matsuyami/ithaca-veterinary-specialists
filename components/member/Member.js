bc53c7e41cd708a1238db2485c9d20a8e0457be4
Naji <kado@pop-os.localdomain>
2022-10-21 22:37:54 -0700

    feat: team members now show in grid

--- components/member/Member.js
+++ components/member/Member.js
@@ -2,10 +2,29 @@ import styles from './Member.module.css'
 
 export const Member = () => {
   return (
-    <div className={styles.member}>
-      <div className={styles.member__container}>
+    <div className={styles.member__container}>
+      <div className={styles.member}>
         <img src='/images/dr-dan-hoffman.jpg' className={styles.member__image}
-        alt='ithaca doctor' fetchPriority='low' loading='lazy' decoding='async'/>
+        alt='ithaca doctor' fetchpriority='low' loading='lazy' decoding='async'/>
+        <div className={styles.name}>Dr. Dan Hoffman </div>
+        <div className={styles.title}> Veterinarian </div>
+      </div>
+
+      <div className={styles.member}>
+        <img src='/images/flower_dog.jpg' className={styles.member__image}
+        alt='ithaca doctor' fetchpriority='low' loading='lazy' decoding='async'/>
+        <div className={styles.name}>Dr. Dan Hoffman </div>
+        <div className={styles.title}> Veterinarian </div>
+      </div>
+      <div className={styles.member}>
+        <img src='/images/teapot_dog.jpg' className={styles.member__image}
+        alt='ithaca doctor' fetchpriority='low' loading='lazy' decoding='async'/>
+        <div className={styles.name}>Dr. Dan Hoffman </div>
+        <div className={styles.title}> Veterinarian </div>
+      </div>
+      <div className={styles.member}>
+        <img src='/images/dr-dan-hoffman.jpg' className={styles.member__image}
+        alt='ithaca doctor' fetchpriority='low' loading='lazy' decoding='async'/>
         <div className={styles.name}>Dr. Dan Hoffman </div>
         <div className={styles.title}> Veterinarian </div>
       </div>
--- pages/team/index.js
+++ pages/team/index.js
@@ -10,6 +10,7 @@ export default function Team() {
     hoverColor: 'var(--primary-clr)',
     url: '/contact'
   }
+
   return(
     <>
       <Header/>
