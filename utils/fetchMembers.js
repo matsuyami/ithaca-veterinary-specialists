6f6967be0772e5ad4f3d78ed653c7a7909ac1c06
matsuyami <mkadwaki@gmail.com>
2022-11-10 00:25:48 -0800

    fix(deployment): use vercel url

--- utils/fetchMembers.js
+++ utils/fetchMembers.js
@@ -1,9 +1,7 @@
 export const fetchMembers = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getMembers`)
+  const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getMembers`)
   const data = await res.json()
   const members = data.members
-
-  console.log('fetching', members)
 
   return members
 }
