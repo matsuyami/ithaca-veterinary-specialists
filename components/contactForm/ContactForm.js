73654e1d903537779f9bd22a109f5ea42c0a237e
matsuyami <mkadwaki@gmail.com>
2022-11-19 17:05:47 -0800

    fix(ui): better form ui

--- components/contactForm/ContactForm.js
+++ components/contactForm/ContactForm.js
@@ -3,21 +3,33 @@ import styles from './ContactForm.module.css'
 
 export const ContactForm = () => {
   return (
-    <form className={styles.contactForm} action="/send-data-here" method="post">
-      <div className={styles.contactForm__info}>
-        <label className={styles.contactForm__label} htmlFor="name">Name</label>
-        <input className={styles.contactForm__input} type="text" name="Name" />
-
-        <label className={styles.contactForm__label} htmlFor="email">Email</label>
-        <input className={styles.contactForm__input} type="text" name="Email" />
+    <div className={styles.contactForm}>
+      <div className={styles.contactForm__heading}>
+        <h2 className={styles.contactForm__header}>Let's Talk</h2>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <image href="/images/waving_emoji.svg" height="40" width="40" />
+        </svg>
       </div>
+      <form className={styles.contactForm_form} action="/send-data-here" method="post">
+        <div className={styles.contactForm__info}>
+          <div>
+            <label className={styles.contactForm__label} htmlFor="name">Name</label>
+            <input placeholder='Name' className={styles.contactForm__input} type="text" name="Name" />
+          </div>
 
-      <label className={styles.contactForm__label} htmlFor="message" name="Messaage">Message</label>
-      <textarea className={styles.contactForm__textArea}></textarea>
+          <div>
+            <label className={styles.contactForm__label} htmlFor="email">Email</label>
+            <input placeholder='Email' className={styles.contactForm__input} type="text" name="Email" />
+          </div>
+        </div>
 
-      <div className={styles.contactForm__button}>
-        <input type='submit'/>
-      </div>
-    </form>
+        <label className={styles.contactForm__label} htmlFor="message" name="Messaage">Message</label>
+        <textarea placeholder='Please enter your message here' className={styles.contactForm__textArea}></textarea>
+
+        <div className={styles.contactForm__button}>
+          <input type='submit'/>
+        </div>
+      </form>
+    </div>
   )
 }
