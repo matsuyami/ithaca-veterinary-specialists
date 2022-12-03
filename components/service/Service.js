c3da0a38c3463c2ecce8a267369da70d1b78d54f
matsuyami <mkadwaki@gmail.com>
2022-12-03 15:10:17 -0800

    feat: services ui

--- components/service/Service.js
+++ components/service/Service.js
@@ -1,9 +1,30 @@
+import { useState } from 'react'
 import styles from './Service.module.css'
 
-export default Service = () => {
-  return (
-    <div className={styles.service}>
+export const Service = ({children}) => {
+  const [clicked, setClicked] = useState(false)
 
+  return (
+    <div className={styles.service} onClick={() => setClicked(prevClicked => !prevClicked)}>
+      <div className={styles.service__box}>
+        <div className={styles.service__name}> {children} 
+          <div className={styles.service__arrow}>
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <image href="/images/down-arrow.svg" height="24" width="24" />
+            </svg>
+          </div>
+        </div>
+        { clicked && 
+          <p className={styles.service__details}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet eros sodales, ultrices ante et, 
+              vulputate nulla. Cras ultricies velit erat, vel faucibus justo iaculis sit amet. Integer ac lectus ut risus 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet eros sodales, ultrices ante et, 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet eros sodales, ultrices ante et, 
+              vulputate nulla. Cras ultricies velit erat, vel faucibus justo iaculis sit amet. Integer ac lectus ut risus 
+              vulputate nulla. Cras ultricies velit erat, vel faucibus justo iaculis sit amet. Integer ac lectus ut risus 
+          </p>
+        }
+      </div>
     </div>
   )
 }
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,41 +1,56 @@
-import Head from "next/head"
-import { Footer } from "../../components/footer/Footer"
-import { Header } from "../../components/header/Header"
+import Head from 'next/head'
+import { Footer } from '../../components/footer/Footer'
+import { Header } from '../../components/header/Header'
+import { Service } from '../../components/service/Service'
+import { Hero } from '../../components/hero/Hero'
 import styles from './Services.module.css'
 
 export default function Services(){
+  const buttonOptions = {
+    bg: 'var(--blue)',
+    hoverColor: 'var(--primary-clr)',
+    url: '/contact'
+  }
+
   return(
     <div> 
       <Head>
         <title>Ithaca Veterinary Specialists | Services </title> 
       </Head>
       <Header/>
+      <Hero title='Our Team' subHeading='Lorem, ipsum dolor sit amet consectetur adipisicing elit.' 
+      imageUrl='/images/couch-dog.jpg' backgroundPos='center' buttonOptions={buttonOptions}/>  
       <section className={styles.servicesInfo}>
         <h2 className={styles.servicesInfo__header}> Lorem la ipsum le dolor sit ame </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet eros sodales, ultrices ante et, 
+          vulputate nulla. Cras ultricies velit erat, vel faucibus justo iaculis sit amet. Integer ac lectus ut risus 
+          rhoncus convallis. In purus dolor, facilisis nec ex eget, pulvinar rutrum mi. Donec blandit turpis sed mattis ullamcorper. 
+          Nunc mollis risus et erat suscipit, eget fermentum felis cursus. Quisque lacus purus, venenatis sed justo id, 
+          tristique condimentum arcu. Proin placerat lorem metus, quis venenatis sapien mattis consectetur. Duis egestas consectetur 
+          diam, ac mattis purus interdum mattis. Suspendisse eu mauris vitae tellus interdum pellentesque. Curabitur 
+          bibendum, metus ac faucibus dapibus, arcu lectus pretium odio, non iaculis nulla 
+          felis id dolor. Vivamus sit amet hendrerit arcu. Pellentesque vestibulum nec lacus ac porta. Nam maximus turpis sit amet mollis egestas.
+        </p>
+        <p>
+          Aenean mauris sem, commodo eu rutrum id, ultrices in arcu. Integer nec velit augue. Phasellus vel feugiat diam. Duis ac 
+          sodales dui. Donec cursus diam commodo felis aliquam luctus vel pellentesque orci. Fusce ut lobortis augue. Fusce pretium 
+          fringilla erat. Donec leo orci, molestie non lorem at, commodo efficitur ante. Nam sagittis ornare mauris eu semper. 
+        </p>
+        <p>
+        Integer et metus hendrerit, tempor nibh viverra tellus. Curabitur tristique 
+        euismod neque, non volutpat massa dignissim elementum. Cras gravida augue dui, eget 
+        vestibulum metus convallis eu. Sed egestas sapien facilisis nibh gravida, in mollis ligula egestas.
+        Suspendisse facilisis finibus felis, vitae mattis turpis blandit a. Nunc ut condimentum lorem. Quisque commodo aliquam dolor, 
+        id mollis augue faucibus sit amet. Nunc porta turpis et nisl maximus cursus. Ut posuere enim ex, eu faucibus elit mollis sed. 
+        In nec fermentum nisi. Nulla viverra faucibus nunc. Sed ut ante eu felis ultricies placerat.
+        </p>
         <div className={styles.servicesInfo__content}> 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet eros sodales, ultrices ante et, 
-            vulputate nulla. Cras ultricies velit erat, vel faucibus justo iaculis sit amet. Integer ac lectus ut risus 
-            rhoncus convallis. In purus dolor, facilisis nec ex eget, pulvinar rutrum mi. Donec blandit turpis sed mattis ullamcorper. 
-            Nunc mollis risus et erat suscipit, eget fermentum felis cursus. Quisque lacus purus, venenatis sed justo id, 
-            tristique condimentum arcu. Proin placerat lorem metus, quis venenatis sapien mattis consectetur. Duis egestas consectetur 
-            diam, ac mattis purus interdum mattis. Suspendisse eu mauris vitae tellus interdum pellentesque. Curabitur 
-            bibendum, metus ac faucibus dapibus, arcu lectus pretium odio, non iaculis nulla 
-            felis id dolor. Vivamus sit amet hendrerit arcu. Pellentesque vestibulum nec lacus ac porta. Nam maximus turpis sit amet mollis egestas.
-          </p>
-          <p>
-            Aenean mauris sem, commodo eu rutrum id, ultrices in arcu. Integer nec velit augue. Phasellus vel feugiat diam. Duis ac 
-            sodales dui. Donec cursus diam commodo felis aliquam luctus vel pellentesque orci. Fusce ut lobortis augue. Fusce pretium 
-            fringilla erat. Donec leo orci, molestie non lorem at, commodo efficitur ante. Nam sagittis ornare mauris eu semper. 
-          </p>
-          <p>
-          Integer et metus hendrerit, tempor nibh viverra tellus. Curabitur tristique 
-          euismod neque, non volutpat massa dignissim elementum. Cras gravida augue dui, eget 
-          vestibulum metus convallis eu. Sed egestas sapien facilisis nibh gravida, in mollis ligula egestas.
-          Suspendisse facilisis finibus felis, vitae mattis turpis blandit a. Nunc ut condimentum lorem. Quisque commodo aliquam dolor, 
-          id mollis augue faucibus sit amet. Nunc porta turpis et nisl maximus cursus. Ut posuere enim ex, eu faucibus elit mollis sed. 
-          In nec fermentum nisi. Nulla viverra faucibus nunc. Sed ut ante eu felis ultricies placerat.
-          </p>
+          <div className={styles.servicesInfo__list}>
+            <Service>Neurology</Service>
+            <Service>Cardiology</Service>
+            <Service>Potato</Service>
+          </div>
         </div>
       </section>
       <Footer/>
