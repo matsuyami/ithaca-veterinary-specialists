83bb5905f4b05c117895fd44bb61aa6b32a7083d
matsuyami <mkadwaki@gmail.com>
2022-12-04 09:28:43 -0800

    fix: body bg is now white

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,6 +3,7 @@ import { Footer } from "../../components/footer/Footer";
 import { Header } from "../../components/header/Header";
 import { Hero } from "../../components/hero/Hero";
 import Head from "next/head";
+import { useEffect } from "react";
 
 export default function Contact() {
   const buttonOptions = {
@@ -10,6 +11,14 @@ export default function Contact() {
     hoverColor: 'var(--primary-clr)',
     url: '/contact'
   }
+
+  useEffect( () => {
+    function changeBodyBg() {
+      document.body.style.backgroundColor = 'white' 
+    }
+    changeBodyBg()
+  }, [])
+
   return(
     <div>
       <Head>
